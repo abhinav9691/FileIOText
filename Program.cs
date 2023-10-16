4c233941bece843fa34747ee1920a9fae2336ed7
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-16 08:54:16 +0530

    [Abhinav]-Adding FileIO part

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOOperationsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //fileExists();
+            //ReadAllLines();
+            //ReadAllText();
+
+            StreamReaderWriterOps readThroughStreamReader = new StreamReaderWriterOps();
+            //readThroughStreamReader.WriteUsingStreamWriter();
+            readThroughStreamReader.ReadFromStreamReader();
+        }
+
+        public static void fileExists()
+        {
+            //String path = @"/c/Users/dell/source/repos/FileIOOperationsDemo/FileIOOperationsDemo/Example.txt";
+            String path = @"C:\Users\dell\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.ReadKey();
+        }
+
+        public static void ReadAllLines()
+        {
+            String path = @"C:\Users\dell\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+
+            String[] lines;
+
+            lines = File.ReadAllLines(path);
+
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+
+            Console.ReadKey();
+
+        }
+        public static void ReadAllText()
+        {
+            String path = @"C:\Users\dell\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+
+            String lines;
+
+            lines = File.ReadAllText(path);
+
+            Console.WriteLine(lines);
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
---
+++ StreamReaderWriterOps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperationsDemo
+{
+    public class StreamReaderWriterOps
+    {
+        public void WriteUsingStreamWriter()
+        {
+            String path = @"C:\Users\dell\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hello world - .Net is awesome");
+                sr.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+        }
+
+        public void ReadFromStreamReader()
+        {
+            String path = @"C:\Users\dell\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+
+                while((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey() ;
+        }
+    }
+}
